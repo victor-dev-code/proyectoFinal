fd7ce1b50a2dc483091f7cb0a043543a157bb8f4
josu989 <josu989@outlook.es>
2021-05-19 17:43:59 -0500

    .

--- GestionClaves/GestionClaves/views.py
+++ GestionClaves/GestionClaves/views.py
@@ -6,6 +6,7 @@ import os, html, re, string, requests, base64, sys, datetime
 from pagina1 import models
 from GestionClaves.decoradores import login_requerido
 from .bot import mandar_mensaje_bot
+from datetime import timezone
 
 def ip_cliente(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -16,7 +17,32 @@ def ip_cliente(request):
     return ip
 
 def intento_ip(ip):
-    
+    guardar_registro = models.IntentosIP.objects.filter(llave_primaria=ip)
+    if not guardar_registro:
+        registro = models.IntentosIP(ip=ip, cont=1, last_Peticion=datetime.datetime.now())
+        registro.save()
+        return True
+    guardar_registro = guardar_registro[0]
+    diferencia_tiempo = tiempo_ahora(guardar_registro.last_Peticion)
+    if diferencia_tiempo > 60:
+        guardar_registro.last_Peticion = datetime.datetime.now()
+        guardar_registro.cont = 1
+        guardar_registro.save()
+        return True
+    else:
+        if guardar_registro.cont < 3:
+             guardar_registro.last_Peticion = datetime.datetime.now()
+             guardar_registro.cont += 1
+             guardar_registro.save()
+             return True
+        else:
+            guardar_registro.last_Peticion = datetime.datetime.now()
+            return False
+
+def tiempo_ahora(tiempo):
+    ahora =  datetime.datetime.now(timezone.utc)
+    diferencia = ahora - tiempo
+    return diferencia.seconds
 
 @login_requerido
 def token(request):
@@ -42,8 +68,6 @@ def login(request):
     elif request.method == 'POST':
         nick = request.POST.get('nick', '').strip()
         password = request.POST.get('contraseña', '').strip()
-
-        
         nick = html.escape(nick)
         password = html.escape(password)
  #       password = validar_password(password)
