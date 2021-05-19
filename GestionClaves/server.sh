3c8002b92011676451a953859bb6575abf68a77d
josu989 <josu989@outlook.es>
2021-05-19 14:30:30 -0500

    .

--- GestionClaves/server.sh
+++ GestionClaves/server.sh
@@ -5,7 +5,8 @@ do
 echo "elige una opcion"
 echo "1.-levantar servidor"
 echo "2.-relizar migraciones"
-echo "3.-salir"
+echo "3.-crear app"
+echo "4.-salir"
 read OPC 
 case $OPC in
 1)
@@ -31,6 +32,18 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 ;;
 3)
+echo "escribe el nombre del archvo env"
+read FILE
+echo "escribe el nombre de la aplicacion"
+read app
+[[ -f $FILE ]] || { echo se esperaba como primer archivo env ; exit 1; }
+for linea in $(ccdecrypt -c $FILE); do
+        echo export $linea
+        export $linea
+done
+python3 manage.py startapp $app
+;;
+4)
 exit
 ;;
 *)
